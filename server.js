beed939ed128a85c0ebe52cba0fa578b917aa3c2
Donnella14 <mugishadonnella14@gmail.com>
2021-08-04 17:43:58 +0200

    hello

--- server.js
+++ server.js
@@ -13,9 +13,16 @@ app.use(bodyParser.json());
 app.use("/freemantor/v1/user", userRouter);
 app.use("/freemantor/v1/session", sessionRouter);
 
+app.use('/',(req,res)=>{
+    res.status(404).send({
+    status:404,
+    message:"this routes doesn't exist"
+    })
+});
+
 
 const databaseUrl=process.env.DATABASE;
-mongoose.connect( databaseUrl,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:false}).then(()=>console.log("Database connected succesfully")); 
+mongoose.connect(databaseUrl,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:false}).then(()=>console.log("Database connected succesfully")); 
 //port:4040
 const port= process.env.PORT;
 app.listen(4040, ()=>{
