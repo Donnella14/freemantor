2ee0e933bf7b99b0344d07a21867c91d14b5eb10
Donnella14 <mugishadonnella14@gmail.com>
2021-08-18 22:34:44 +0200

    last feature

--- server/models/session.js
+++ server/models/session.js
@@ -3,8 +3,8 @@ import mongoose  from "mongoose";
 const Session = new mongoose.Schema({
     title: {type:String,required:[true,"The title is required"]},
     description: String,
-    user:{type:mongoose.Schema.ObjectId, ref:"user"},
-    mentor:{type:mongoose.Schema.ObjectId, ref:"user"},
+    user:{type:mongoose.Schema.ObjectId, ref:"User"},
+    mentor:{type:mongoose.Schema.ObjectId, ref:"User"},
     timeToStart:String,
     timeToEnd:String,
     status:{
@@ -12,6 +12,16 @@ const Session = new mongoose.Schema({
         enum:["appending","approve","decline"], default:"appending"}
 
 });
+Session.pre(/^find/, function(next){
+    this.populate({
+        path:"user",
+        select:"firstName lastName email phone gender"
+    }).populate({
+        path:"mentor",
+        select:"firstName lastName email phone gender"
+    });
+    next();
+})
 const SessionInfo= mongoose.model("session" ,Session);
 
 export default SessionInfo;
